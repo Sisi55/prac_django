20e70233a8a2ee0a5a085d781437cfb8be2291bd
sisi <dqgsh1055@gmail.com>
2019-07-30 15:42:11 +0900

    가장 간단한 서버 만들기

---
+++ ch3/mysite/urls.py
@@ -0,0 +1,28 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from polls import views # polls 폴더 안에 views.py 파일 있다
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+# path(url패턴 문자열,  호출되는 뷰 함수, 추가인자, name=url패턴이름지정)
+    path('polls/', views.index, name='index'), #url 패턴 매칭은 위-아래로 진행되므로, 정의 순서에 유의한다
+    # path('polls/<int:question_id>/', views.detail, name='detail'), #뷰 함수에 인자가 전달된다
+    # path('polls/<int:question_id>/results/', views.results, name='results'),
+    # path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+]
---
+++ ch3/polls/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from polls.models import Question #Question 테이블에 접근하기 위해 import
+
+# Create your views here.
+
+def index(request): #request 객체는 뷰 함수의 필수 인자
+    # lastest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    # context = {'lastest_question_list': lastest_question_list}
+    return render(request, 'polls/index.html', {}) #단순히 페이지 render만 한다
+    #render 디폴트 .html 파일 위치가 polls-앱/template 폴더인가보다
+    #HttpResponse 객체를 반환합니다
+
+
